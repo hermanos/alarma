daefc180195126de54ccc447582add59bc4ff818
sergius0301 <sergius0301@gmail.com>
2013-07-01 14:51:19 +0300

    create scaffould users

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130701110936) do
+ActiveRecord::Schema.define(version: 20130701114403) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "email"
     t.string   "password"
-    t.string   "name"
-    t.string   "photo"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
